e1726c793e8063842f254ef484d765931e4dae75
Samuel Shivy <sshivy7@gmail.com>
2025-02-18 18:49:56 -0500

    inserted router into nav page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavigationPageComponent } from './components/navigation-page/navigation-page.component';
 
-const routes: Routes = [
-    { path: '', pathMatch: 'full', component: NavigationPageComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],

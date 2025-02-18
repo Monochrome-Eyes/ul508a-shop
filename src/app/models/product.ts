373e3273f3c50d986620b8be8e08608b3739b799
Samuel Shivy <sshivy7@gmail.com>
2025-02-17 21:37:51 -0500

    data model created. Products not created

---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    id: number = 0;
+    name: string = 'placeholder';
+    image: string = 'imageURL';
+    price: number = 0;
+    category: string = 'placeholder';
+    nomCurrentRating: number = 0;
+    isULListed: boolean = false;
+}

3b1fc361fe82194293ca97d8e680f1b58d7c3663
Samuel Shivy <sshivy7@gmail.com>
2025-02-18 18:47:45 -0500

    generated home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavigationPageComponent } from './components/navigation-page/navigation-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', pathMatch: 'full', component: NavigationPageComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-    declarations: [AppComponent, NavigationPageComponent],
+    declarations: [AppComponent, NavigationPageComponent, HomeComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,

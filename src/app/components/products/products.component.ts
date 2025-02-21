6c6586f7c3acb3626f2707b1463c93fc915ea067
Samuel Shivy <sshivy7@gmail.com>
2025-02-20 22:03:20 -0500

    working on products page

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+    products: Product[] = [];
 
+    constructor(private productService: ProductsService) {}
+
+    ngOnInit(): void {
+        this.productService.getAllProducts().subscribe((response) => {
+            this.products = response;
+        });
+    }
 }
